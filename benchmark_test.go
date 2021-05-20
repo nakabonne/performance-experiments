3d0143283ff7794b9fb0f5f593fb9a3c5e80f637
nakabonne <ryo@nakao.dev>
2021-05-20 16:14:55 +0900

    Move structs to test

--- benchmark_test.go
+++ benchmark_test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func BenchmarkSliceAppend(b *testing.B) {
-	s := &Slice{s: []*Value{}}
+	s := []*Value{}
 	for i := 0; i < b.N; i++ {
-		s.Append(&Value{})
+		s = append(s, &Value{})
 	}
 }
 
@@ -14,4 +14,29 @@ func BenchmarkLinkedListAppend(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		list.Append(&Value{})
 	}
+}
+
+type Value struct{}
+
+type LinkedList struct {
+	head *Node
+	tail *Node
+}
+
+type Node struct {
+	val  *Value
+	next *Node
+	prev *Node
+}
+
+func (l *LinkedList) Append(v *Value) {
+	newNode := &Node{val: v}
+	if l.tail == nil {
+		l.head = newNode
+		l.tail = newNode
+		return
+	}
+	newNode.prev = l.tail
+	l.tail.next = newNode
+	l.tail = newNode
 }
--- main.go
+++ main.go
@@ -1,39 +1,4 @@
 package main
 
-type Value struct{}
-
-// Slice
-type Slice struct {
-	s []*Value
-}
-
-func (s *Slice) Append(t *Value) {
-	s.s = append(s.s, t)
-}
-
-// LinkedList
-type LinkedList struct {
-	head *Node
-	tail *Node
-}
-
-type Node struct {
-	val  *Value
-	next *Node
-	prev *Node
-}
-
-func (l *LinkedList) Append(v *Value) {
-	newNode := &Node{val: v}
-	if l.tail == nil {
-		l.head = newNode
-		l.tail = newNode
-		return
-	}
-	newNode.prev = l.tail
-	l.tail.next = newNode
-	l.tail = newNode
-}
-
 func main() {
 }
