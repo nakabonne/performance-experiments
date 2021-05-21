f9129ed2fdbb3ff8bee1dec6073743da3d182c66
nakabonne <ryo@nakao.dev>
2021-05-21 18:47:14 +0900

    Add iterate test

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func BenchmarkSliceAppend(b *testing.B) {
 	s := []*Value{}
@@ -16,7 +18,38 @@ func BenchmarkLinkedListAppend(b *testing.B) {
 	}
 }
 
-type Value struct{}
+func BenchmarkSliceIterate(b *testing.B) {
+	s := []*Value{}
+	for i := 0; i < 100000; i++ {
+		s = append(s, &Value{id: i})
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for i := range s {
+			v := s[i]
+			_ = v
+		}
+	}
+}
+
+func BenchmarkLinkedListIterate(b *testing.B) {
+	list := &LinkedList{}
+	for i := 0; i < 100000; i++ {
+		list.Append(&Value{id: i})
+	}
+	iterator := list.NewIterator()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for iterator.Next() {
+			v := iterator.current.val
+			_ = v
+		}
+	}
+}
+
+type Value struct {
+	id int
+}
 
 type LinkedList struct {
 	head *Node
@@ -39,4 +72,28 @@ func (l *LinkedList) Append(v *Value) {
 	newNode.prev = l.tail
 	l.tail.next = newNode
 	l.tail = newNode
+}
+
+func (l *LinkedList) NewIterator() *Iterator {
+	head := l.head
+	// Put a dummy node so that it positions the head on the first Next() call.
+	dummy := &Node{
+		next: head,
+	}
+	return &Iterator{
+		current: dummy,
+	}
+}
+
+type Iterator struct {
+	current *Node
+}
+
+func (i *Iterator) Next() bool {
+	if i.current == nil {
+		return false
+	}
+	next := i.current.next
+	i.current = next
+	return i.current != nil
 }
